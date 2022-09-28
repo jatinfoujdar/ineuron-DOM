f249e650a72c0d20bdf7b3fa1465d04f1397247d
jatin foujdar <53086199+jatinfoujdar@users.noreply.github.com>
2022-09-28 20:51:27 +0530

    Add files via upload

---
+++ dom-fsjs/code.js
@@ -0,0 +1,7 @@
+//1
+//document.querySelector(".side-bar .crayons-card .crayons-subtitle-2").innerHTML="jatin foujdar";
+// document.querySelector(".side-bar .crayons-card .color-base-70").innerHTML="i write code";
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+//2
